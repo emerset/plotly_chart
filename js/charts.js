cb8d60522bb442caf39d423483a938dfa4206b8c
Emerset Farquharson <emersetfarquharson@Emersets-Laptop.ht.home>
2022-11-29 23:32:19 -0500

    Deliverable 3

--- js/charts.js
+++ js/charts.js
@@ -122,7 +122,59 @@ function buildCharts(sample) {
 
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot('plotArea2', bubbleData, bubbleLayout); 
+
+    // Deliverable 3
+
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    // Create a variable that holds the first sample in the array.
+    var result = resultArray[0];
+    console.log(result);
+    // 2. Create a variable that holds the first sample in the metadata array.
+      
+
+    // Create variables that hold the otu_ids, otu_labels, and sample_values.
+
+
+    // 3. Create a variable that holds the washing frequency.
+    var wfreq = result.wfreq;
+    // Create the yticks for the bar chart.
+    // 4. Create the trace for the gauge chart.
+    var gaugeData = [
+      {
+        value: wfreq,
+        title: { text: "<b>Belly Button Washing Frequency</b><br />Scrubs per Week"},
+        type: "indicator",
+        mode: "gauge+number",
+        gauge: {
+          axis: { range: [null, 10]},
+          bar: {color: "black"},
+          steps: [
+            { range: [0, 2], color: "red" },
+            { range: [2, 4], color: "orange" },
+            { range: [4, 6], color: "yellow" },
+            { range: [6, 8], color: "lime" },
+            { range: [8, 10], color: "green" }
+          ]
+        }
+      }
+      
+    ];
+    
+    // 5. Create the layout for the gauge chart.
+    var gaugeLayout = { 
+      width: 500,
+      height: 400,
+      margin: {t: 0, b: 0 }
+    };
+
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot('gauge', gaugeData, gaugeLayout);
+
   });
+
+  
 }
 
 
